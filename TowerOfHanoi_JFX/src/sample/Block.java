fbb082355d61e17b6f34962b4169c32fde6aee18
Zaid Careem <64317607+zaidcareem@users.noreply.github.com>
2021-07-26 22:48:02 +0530

    bug fix

---
+++ TowerOfHanoi_JFX/src/sample/Block.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.scene.shape.Rectangle;
+
+public class Block {
+
+    public Rectangle block;
+    public int width;
+
+    Block(Rectangle block, int width) {
+
+        this.width = width;
+        this.block = block;
+    }
+}
